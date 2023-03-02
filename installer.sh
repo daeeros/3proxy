cfdf699433980d7a7629eb542073ac11873f205a
Lumo <34482855+daeeros@users.noreply.github.com>
2023-03-02 16:35:19 +0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
-echo "Usage: $0 username password"
-exit 1
-fi
 
 apt-get update && apt-get -y upgrade
-apt-get install gcc make git -y
+apt-get install gcc make git curl -y
 git clone https://github.com/z3apa3a/3proxy
 cd 3proxy
 ln -s Makefile.Linux Makefile
 make
 sudo make install
-echo "" > /etc/3proxy/conf/passwd && echo "$1:CL:$2" >> /etc/3proxy/conf/passwd
+
+echo ""
+read -p "Enter the username: " username
+read -sp "Enter the password: " password
+echo ""
+
+echo "" > /etc/3proxy/conf/passwd && echo "$username:CL:$password" >> /etc/3proxy/conf/passwd
+
+current_ipv4=$(curl -s https://api.ipify.org)
+
+echo ""
+echo "Proxy link: http://$username:$password@$current_ipv4:3128"
+echo ""
+
 ufw allow 22/tcp
 ufw allow 3128/tcp
 yes | ufw enable
